b9b78ca74721c3aac3b2d8452084bafe019fa2e4
Theeban <69495468+Theebanraj-Uthayakumar@users.noreply.github.com>
2021-09-23 23:59:30 +0530

    Booking Discount Calculation implementation

--- app/src/main/java/com/example/mad/Booking_1.java
+++ app/src/main/java/com/example/mad/Booking_1.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Booking_1 extends AppCompatActivity {
 
@@ -17,6 +23,21 @@ public class Booking_1 extends AppCompatActivity {
     Button button;
     //Cancel Button
     Button button8;
+
+    private  Spinner spnBrand;
+    private EditText txtQuantity;
+    private TextView txtdiscount;
+    private TextView txttotal;
+
+    String[] brands = {"Le Bernardin","City Grocery","Belcanto","Piazza Duomo","Odette"};
+
+    double value;
+    int quantity;
+    double totalValue;
+    double discountValue;
+
+
+    String selectedBrand;
 
     EditText checkin_date_input, chekin_time_input, numberof_cabin_input, email_input, mobile_number;
 
@@ -32,6 +53,88 @@ public class Booking_1 extends AppCompatActivity {
 //                back();
 //            }
 //        });
+
+        spnBrand = findViewById(R.id.spnBrand);
+        txtQuantity = findViewById(R.id.txtquantity);
+        txtdiscount = findViewById(R.id.txtdiscount);
+        txttotal  = findViewById(R.id.txttotal);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,brands);
+
+        spnBrand.setAdapter(adapter);
+
+        spnBrand.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedBrand = brands[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        txtQuantity.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    Log.d("onItemSelected:", selectedBrand);
+                    quantity = Integer.parseInt(txtQuantity.getText().toString());
+                    if (selectedBrand.equals("Le Bernardin")) {
+                        value = 5000;
+                        if (quantity > 5) {
+                            discountValue = (value * 0.05);
+                            totalValue = (value * quantity) - discountValue;
+                        } else {
+                            totalValue = (value * quantity);
+                        }
+                    } else if (selectedBrand.equals("City Grocery")) {
+                        value = 3000;
+                        if (quantity > 5) {
+                            discountValue = (value * 0.05);
+                            totalValue = (value * quantity) - discountValue;
+                        } else {
+                            totalValue = (value * quantity);
+                        }
+                    } else if (selectedBrand.equals("Belcanto")) {
+                        value = 2000;
+                        if (quantity > 5) {
+                            discountValue = (value * 0.05);
+                            totalValue = (value * quantity) - discountValue;
+                        } else {
+                            totalValue = (value * quantity);
+                        }
+                    } else if (selectedBrand.equals("Piazza Duomo")) {
+                        value = 1000;
+                        if (quantity > 5) {
+                            discountValue = (value * 0.05);
+                            totalValue = (value * quantity) - discountValue;
+                        } else {
+                            totalValue = (value * quantity);
+                        }
+                    } else if (selectedBrand.equals("Odette")) {
+                        value = 4000;
+                        if (quantity > 5) {
+                            discountValue = (value * 0.05);
+                            totalValue = (value * quantity) - discountValue;
+                        } else {
+                            totalValue = (value * quantity);
+                        }
+                    } else {
+                        try {
+                            throw new Exception("user not selected");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    txtdiscount.setText(String.valueOf(discountValue));
+                    txttotal.setText(String.valueOf(totalValue));
+                    return true;
+                }
+                return false;
+            }
+        });
 
         button =  (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -68,7 +171,6 @@ public class Booking_1 extends AppCompatActivity {
                         Integer.valueOf(mobile_number.getText().toString().trim()));
             }
         });
-
     }
 
     public void back(){
