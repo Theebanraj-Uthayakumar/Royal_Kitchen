b29b8d7dc20545df41cd5c7d977797ce8c99f8da
jeryluxshigan07 <IT20105716@my.sliit.lk>
2021-09-24 11:28:31 +0530

    jery5

--- app/src/main/java/com/example/mad/cabin_choose.java
+++ app/src/main/java/com/example/mad/cabin_choose.java
@@ -2,13 +2,31 @@ package com.example.mad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class cabin_choose extends AppCompatActivity {
+
+    TextView textView138;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cabin_choose);
+
+        textView138 =  findViewById(R.id.textView138);
+        textView138.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cabin_choose();
+            }
+        });
+    }
+    public void cabin_choose(){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
