e3cb92912d4b2a0252933d80be26ffa41d61c8bc
Theeban <69495468+Theebanraj-Uthayakumar@users.noreply.github.com>
2021-09-24 13:31:46 +0530

    list view implementation

--- app/src/main/java/com/example/mad/MyDatabaseHelper.java
+++ app/src/main/java/com/example/mad/MyDatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.mad;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
@@ -62,5 +63,16 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         }else {
             Toast.makeText(context, "Your data successfully added!", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    Cursor readAllData(){
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db != null){
+            cursor = db.rawQuery(query,null);
+        }
+        return cursor;
     }
 }
--- app/src/main/java/com/example/mad/View_Booking_Client.java
+++ app/src/main/java/com/example/mad/View_Booking_Client.java
@@ -1,14 +1,63 @@
 package com.example.mad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class View_Booking_Client extends AppCompatActivity {
+
+    RecyclerView recyclearView;
+    MyDatabaseHelper myDB;
+    ArrayList<String > Booking_id, check_date, check_time, cabin_type, number_of_cabin, email, cnumber, discount, fare;
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view__booking__client);
+        recyclearView = findViewById(R.id.recyclerview);
+
+        myDB = new MyDatabaseHelper(View_Booking_Client.this);
+        Booking_id = new ArrayList<>();
+        check_date = new ArrayList<>();
+        check_time = new ArrayList<>();
+        cabin_type = new ArrayList<>();
+        number_of_cabin = new ArrayList<>();
+        email = new ArrayList<>();
+        cnumber = new ArrayList<>();
+        discount = new ArrayList<>();
+        fare = new ArrayList<>();
+
+        storeDataInArrays();
+
+        customAdapter = new CustomAdapter(View_Booking_Client.this, Booking_id, check_date, check_time, cabin_type, number_of_cabin, email, cnumber, discount, fare);
+        recyclearView.setAdapter(customAdapter);
+        recyclearView.setLayoutManager(new LinearLayoutManager(View_Booking_Client.this));
+    }
+
+    void storeDataInArrays(){
+        Cursor cursor = myDB.readAllData();
+        if(cursor.getCount() == 0){
+            Toast.makeText(this,"No data", Toast.LENGTH_SHORT).show();
+        }else{
+            while (cursor.moveToNext()){
+                Booking_id.add(cursor.getString(0));
+                check_date.add(cursor.getString(1));
+                check_time.add(cursor.getString(2));
+                cabin_type.add(cursor.getString(3));
+                number_of_cabin.add(cursor.getString(4));
+                email.add(cursor.getString(5));
+                cnumber.add(cursor.getString(6));
+                discount.add(cursor.getString(7));
+                fare.add(cursor.getString(8));
+            }
+        }
     }
 }
