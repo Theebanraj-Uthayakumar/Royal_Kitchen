fa40da458ab177887bb11191802b5819773834f9
AbinayaThaya <abinayathayaparan99@gmail.com>
2021-09-24 12:42:44 +0530

    abi2

--- app/src/main/java/com/example/mad/MyDatabaseHelper.java
+++ app/src/main/java/com/example/mad/MyDatabaseHelper.java
@@ -24,8 +24,16 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     private static final String COLUMN_DISCOUNT = "discount";
     private static final String COLUMN_FARE = "fare";
 
+    //foods
+    private static final String TABLE_FOODNAME = "Add_foods";
+    private static final String COLUMN_FOODID = "food_id";
+    private static final String COLUMN_FOOD_NAME = "food_name";
+    private static final String COLUMN_FOOD_TYPE = "food_type";
+    private static final String COLUMN_FOOD_DESCRIPTION = "food_description";
+    private static final String COLUMN_FOOD_PRICE = "food_price";
 
-    public MyDatabaseHelper(@Nullable Context context ) {
+
+    public MyDatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
     }
@@ -34,29 +42,53 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_NAME + "(COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, check_in_date TEXT, check_in_time TEXT, number_of_cabins INTEGER, email_address TEXT, mobile_number INTEGER)");
+        db.execSQL("create table " + TABLE_FOODNAME + "(COLUMN_FOODID INTEGER PRIMARY KEY AUTOINCREMENT, food_name TEXT, food_type TEXT, food_description TEXT, food_price TEXT)");
     }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_FOODNAME);
         onCreate(db);
     }
 
-    void Booking(String checkin_date, String checkin_time, int numberofcabin, String email, int number){
+    void Booking(String checkin_date, String checkin_time, int numberofcabin, String email, int number) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
 
-        cv.put(COLUMN_CHECK_IN_DATE,checkin_date);
-        cv.put(COLUMN_CHECK_IN_TIME,checkin_time);
-        cv.put(COLUMN_NUMBER_OF_CABINS,numberofcabin);
-        cv.put(COLUMN_EMAIL,email);
-        cv.put(COLUMN_MOBILE_NUMBER,number);
+        cv.put(COLUMN_CHECK_IN_DATE, checkin_date);
+        cv.put(COLUMN_CHECK_IN_TIME, checkin_time);
+        cv.put(COLUMN_NUMBER_OF_CABINS, numberofcabin);
+        cv.put(COLUMN_EMAIL, email);
+        cv.put(COLUMN_MOBILE_NUMBER, number);
 
-        long result = db.insert(TABLE_NAME,null,cv);
-        if(result == -1){
+
+        long result = db.insert(TABLE_NAME, null, cv);
+        if (result == -1) {
             Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
-        }else {
+        } else {
+            Toast.makeText(context, "Your data successfully added!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //foods
+    void Adding(String foodname, String foodtype, String fooddescription, String foodprice) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_FOOD_NAME, foodname);
+        cv.put(COLUMN_FOOD_TYPE, foodtype);
+        cv.put(COLUMN_FOOD_DESCRIPTION, fooddescription);
+        cv.put(COLUMN_FOOD_PRICE, foodprice);
+
+
+        long result = db.insert(TABLE_FOODNAME, null, cv);
+        if (result == -1) {
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        } else {
             Toast.makeText(context, "Your data successfully added!", Toast.LENGTH_SHORT).show();
         }
     }
 }
+
--- app/src/main/java/com/example/mad/add_food.java
+++ app/src/main/java/com/example/mad/add_food.java
@@ -3,12 +3,57 @@ package com.example.mad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class add_food extends AppCompatActivity {
+
+    //submit
+    Button btn_submit;
+
+    EditText foodname_input, foodtype_input, fooddescription_input, foodprice_input;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_add);
-    }
+
+        foodname_input = (EditText) findViewById(R.id.fname);
+                                foodtype_input = (EditText) findViewById(R.id.ftype);
+                                        fooddescription_input = (EditText)  findViewById(R.id.fdesc);
+                                        foodprice_input = (EditText) findViewById(R.id.fprice);
+                                             btn_submit = (Button) findViewById(R.id.sub1);
+
+
+         btn_submit =  (Button) findViewById(R.id.sub1);
+         btn_submit.setOnClickListener(new View.OnClickListener() {
+
+
+
+         @Override
+     public void onClick(View view) {
+             MyDatabaseHelper myDB = new MyDatabaseHelper(add_food.this);
+                             myDB.Adding(
+                                     foodname_input.getText().toString().trim(),
+                                    foodtype_input .getText().toString().trim(),
+                                     fooddescription_input .getText().toString().trim(),
+                                     foodprice_input .getText().toString().trim());
+         }
+
+
+
+
+                                     });
+
+
+
+
+
 }
+
+}
+
+
+
