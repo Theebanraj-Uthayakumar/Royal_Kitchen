38e775e14ec70b95a5cc28f6aa21118e7350567f
danushan99 <danuthasan08@gmail.com>
2021-09-26 17:21:04 +0530

    act2 updt2

--- app/src/main/java/com/example/mad/MainActivity3.java
+++ app/src/main/java/com/example/mad/MainActivity3.java
@@ -9,7 +9,9 @@ public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main3);
     }
 }
--- app/src/main/java/com/example/mad/MainActivity4.java
+++ app/src/main/java/com/example/mad/MainActivity4.java
@@ -3,12 +3,16 @@ package com.example.mad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity4 extends AppCompatActivity {
+public Button x1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main4);
     }
 }
